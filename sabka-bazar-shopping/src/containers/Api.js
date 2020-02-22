e0f6bc920cd385021746b2bb351f8986301d0398
Sahil Satija <sahil.satija@publicissapient.com>
2020-02-22 11:50:13 +0530

    Mock APIs

--- sabka-bazar-shopping/src/containers/Api.js
+++ sabka-bazar-shopping/src/containers/Api.js
@@ -1,6 +1,6 @@
 export const BannerData = async() => {
     try {
-    const response = await fetch('http://localhost:5000/banners');
+    const response = await fetch('https://www.mocky.io/v2/5e36fb30320000d40cae3d2d');
     let data = await response.json(); 
     data = data.filter((d)=>{ return d.isActive === true}).sort((a,b)=>{ return a.order - b.order}); 
     return data;
@@ -11,7 +11,7 @@ export const BannerData = async() => {
 
 export const CategoriesData = async() => {
     try {
-        const response = await fetch('http://localhost:5000/categories');
+        const response = await fetch('https://www.mocky.io/v2/5e36fc4f3200009921ae3d30');
         let data = await response.json();
         data = data.filter((d)=> {return d.enabled === true}).sort((a,b)=>{return a.order - b.order});
         return data;
@@ -24,7 +24,7 @@ export const CategoriesData = async() => {
 
 export const ProductsData = async() => {
     try {
-        const response = await fetch('http://localhost:5000/products');
+        const response = await fetch('https://www.mocky.io/v2/5e36fc913200005500ae3d32');
         let data = await response.json();
         return data;
     }
@@ -35,7 +35,7 @@ export const ProductsData = async() => {
 
 export const postAddtoCartData = async (data) => {
     try {
-        const apiRes = await fetch('http://localhost:5000/addToCart', {
+        const apiRes = await fetch('https://www.mocky.io/v2/5e36fcd43200009921ae3d34', {
             method: 'post',
             body: data.id
         });
